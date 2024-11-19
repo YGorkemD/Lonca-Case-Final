2b6e6aa304a7ef73f3b96bfe1dcec51563eef39a
Yavuz Görkem Deniz <gorkeemdeniz@outlook.com>
2024-11-20 01:08:12 +0300

    Verilen XML dosyasını Mongo için uygun biçime burada dönüştürülür

--- Case.py
+++ Case.py
@@ -23,3 +23,33 @@ class Product:
         self.images = images
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+
+
+    @staticmethod
+    def extract_detail(description, key):
+        """Belirtilen anahtar ile açıklamadan bilgi çeker."""
+        if key in description:
+            return description.split(f'{key}</strong>')[-1].split('</li>')[0].strip()
+        return ""
+
+    def to_dict(self):
+        """Nesneyi MongoDB için bir sözlüğe dönüştürür."""
+        return {
+            '_id': self.product_id,
+            'stock_code': self.product_id,
+            'color': self.color,
+            'discounted_price': self.discounted_price,
+            'is_discounted': self.is_discounted,
+            'price': self.price,
+            'price_unit': self.price_unit,
+            'product_type': self.product_type,
+            'quantity': self.quantity,
+            'series': self.series,
+            'status': self.status,
+            'fabric': self.fabric,
+            'model_measurements': self.model_measurements,
+            'product_measurements': self.product_measurements,
+            'images': self.images,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at,
+        }
